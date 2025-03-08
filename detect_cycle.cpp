cc500c9063e8ddd7b0513d0af50919ba3f9ec5c3
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-03-08 21:06:45 +0600

    add new detect_cycle

---
+++ detect_cycle.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool vis[105];
+vector<int> adj_list[105];
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        cout<<par<<endl;
+        for (int child : adj_list[par])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis)
+        {
+            bfs(i);
+        }
+    }
+    return 0;
+}
