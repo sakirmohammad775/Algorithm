186fc29619ad467ee8f2fceac80dd193987a4d6a
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-03-10 22:58:32 +0600

    add bellman

--- Bellman_ford.cpp
+++ Bellman_ford.cpp
@@ -1,12 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Edge
+{
+public:
+    int a, b, c;
+    Edge(int a, int b, int c)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
 
-int main() {
+
+int main()
+{
     int n, e;
     cin >> n >> e;
-    while(e--){
-        int a, b,c;
+    vector<Edge>edge_list;
+    while (e--)
+    {
+        int a, b, c;
         cin >> a >> b >> c;
+        edge_list.push_back(Edge(a,b,c));
+    }
+    for(auto ed : edge_list){
+        cout<< ed.a<<"  "<< endl;
     }
     return 0;
 }
