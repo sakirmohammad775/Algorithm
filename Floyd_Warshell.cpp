adda9e21769e2772b0e96fa0faa302dc750de7c9
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-03-11 03:17:56 +0600

    negative weight detect bellman for topic code

---
+++ Floyd_Warshell.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            adj_mat[i][j] =INT_MAX;
+        }
+    }
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_mat[a][b] = c; // directed graph
+        // adj_mat[b][a] = c; undirected graph
+
+
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(adj_mat[i][j]==INT_MAX){
+                cout << "INF" << " ";
+            }
+            else{
+
+                cout<< adj_mat[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
