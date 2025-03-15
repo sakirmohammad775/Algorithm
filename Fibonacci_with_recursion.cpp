494448bba823a61f1e33abee8a2a64e577c85671
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-03-15 14:07:23 +0600

    aadd  fibonacci

---
+++ Fibonacci_with_recursion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n < 2)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+int main()
+{
+    cout << fibonacci(8);
+    return 0;
+}
